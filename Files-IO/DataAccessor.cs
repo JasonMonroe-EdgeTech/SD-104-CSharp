4f646b9b769085c724032d97aea57e21d2c47d1f
JasonMonroe-EdgeTech <jmonroe@ancoraeducation.com>
2017-11-08 22:00:58 -0600

    Add files via upload

---
+++ Files-IO/DataAccessor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files_IO
+{
+    class DataAccessor
+    {
+       
+
+
+        public DataAccessor()
+        { 
+            
+
+            //Try to open text files
+            try
+            {
+                //using block automatically disposes stream after use.
+                using (StreamReader textFile1 = new StreamReader("wordbank.txt"))
+                {
+                    Console.Write(textFile1.ReadLine());
+                }
+                    
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("The following exception occured: " + ex.ToString());
+            }
+            finally
+            {
+                //do some more stuff regardless of whether an exception was thrown. 
+            }
+            
+        }
+        
+        //public string GetWord()
+        //{
+
+        //}
+
+
+    }
+}
---
+++ Files-IO/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable declarations
+            string strChoice = "";
+
+            //menu loop
+            while (strChoice != "3")
+            {
+                //main menu
+                Console.WriteLine("-------Main Menu--------");
+                Console.WriteLine("1.\tAdd Names to a File");
+                Console.WriteLine("2.\tView Names in a File");
+                Console.WriteLine("3.\tExit");
+                strChoice = Console.ReadLine();
+
+
+                //process choice
+                switch (strChoice)
+                {
+                    case "1":
+                        addNames();
+                        break;
+                    case "2":
+                        viewNames();
+                        break;
+                    case "3":
+                        break;
+                    default:
+                        Console.WriteLine(strChoice+" is not a valid choice.");
+                        break;
+                }
+            }
+
+        }//end Main()
+
+        static void addNames()
+        {
+            
+
+        }//end addNames()
+
+        static void viewNames()
+        {
+            DataAccessor dataAccessor = new DataAccessor();
+            Console.ReadLine();
+
+        }//end viewNames()
+    }
+}
